10369aeb0b8b4ded61e49b96896a7cf8a2cec00b
stephenwzl <wangzhilong.stephen@bytedance.com>
2020-07-16 17:15:47 +0800

    deploy  Please enter the commit message for your changes. Lines starting

---
+++ precache-manifest.ab68022bb0547bff1d80712a760a169d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e9f62e0b87fc50197a6fc1422dd16b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bde9220fb992a601cb24",
+    "url": "./static/css/main.bcc66f24.chunk.css"
+  },
+  {
+    "revision": "dc1de5597337b587a2e9",
+    "url": "./static/js/2.c8fdace7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c8fdace7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde9220fb992a601cb24",
+    "url": "./static/js/main.7dc1aa16.chunk.js"
+  },
+  {
+    "revision": "242f8660c3dde2ee62a8",
+    "url": "./static/js/runtime-main.e15a9041.js"
+  }
+]);
